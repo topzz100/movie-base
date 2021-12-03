2e495786798e9d50f4fd7d07b69a10db94749ef8
topzz100 <adenekantope@gmail.com>
2021-12-03 16:17:53 +0100

    fetching api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header'
+import Home from './Components/Home'
 import {GlobalStyles} from './GlobalStyles'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       start here
       <GlobalStyles/>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,36 @@ import API from '../API.js'
 
 const Home = () => {
 const[state, setState] = useState()
-const[error, setError] = useState(false)
+const[error, setError] = useState()
 const[loading, setLoading] = useState(true)
+
+const fetchMovies = async(page, searchTerm = "" ) => {
+  try{
+    setLoading(true)
+   const movies = await API.fetchMovies(searchTerm, page )
+   
+   setState(prev => ({
+        ...prev,
+        movies: {
+          ...movies,
+          results:
+            page > 1
+              ? [...prev.movies.results, ...movies.results]
+              : [...movies.results]
+        },
+        loading: false
+      }));
+      
+  }catch(error){
+    setError(error)
+  }
+  console.log(state)
+}
+
+useEffect(() => {
+  setLoading(false)
+  fetchMovies(1)
+}, [])
 
   return (
     <div>
