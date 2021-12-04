ec7aaee969a83381cf0a815f61fc89febb3e8469
topzz100 <adenekantope@gmail.com>
2021-12-04 16:57:57 +0100

    .

---
+++ src/Components/Thumb/Thumb.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Image} from './Thumb.style'
+
+const Thumb = ({image} ) => {
+  return (
+    <Image src = {image}/>
+      
+    
+  )
+}
+
+export default Thumb
